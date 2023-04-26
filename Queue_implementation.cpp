12ecb10399479a983a4a81bad07269ef398abc5b
Ashish Garg <122619955+Ashish-Garg06@users.noreply.github.com>
2023-04-26 14:22:25 +0530

    Added comments

--- Queue_implementation.cpp
+++ Queue_implementation.cpp
@@ -14,10 +14,14 @@ public:
     }
 
     void dequeue() {
+        // Check if the queue is empty
         if (front == -1 && rear == -1) {
             cout << "Queue Underflow: deletion not allowed\n";
         } else {
+            // Increment the front index to remove the element
             front++;
+            // If the front index has crossed the rear index,
+            // reset both front and rear to -1 to indicate an empty queue
             if (front > rear) {
                 front = rear = -1;
             }
@@ -25,12 +29,15 @@ public:
     }
 
     void enqueue(int y) {
+        // Check if the queue is full
         if (rear == S - 1) {
             cout << "Queue overflow: addition not allowed\n";
         } else {
+            // Increment the rear index and add the element to the queue
             rear++;
             arr[rear] = y;
             cout<<"Element "<<y << " was added to the queue."<<endl;
+            // If the front index is still -1, update it to 0
             if (front == -1) {
                 front = 0;
             }
@@ -38,9 +45,11 @@ public:
     }
 
     void display() {
+        // Check if the queue is empty
         if (front == -1 && rear == -1) {
             cout << "Queue is empty.\n";
         } else {
+            // Print the elements in the queue
             cout << "Elements in the queue: ";
             for (int i = front; i <= rear; i++) {
                 cout << arr[i] << " ";
@@ -51,17 +60,24 @@ public:
 };
 
 int main() {
+    // Create a new instance of the Queue class
     Queue q;
+    // Test dequeue operation on an empty queue
     q.dequeue();
+    // Add elements to the queue
     q.enqueue(1);
     q.enqueue(2);
     q.enqueue(3);
     q.enqueue(4);
     q.enqueue(5);
+    // Test adding an element to a full queue
     q.enqueue(6);
+    // Display the elements in the queue
     q.display();
+    // Remove two elements from the queue
     q.dequeue();
     q.dequeue();
+    // Display the elements in the queue again
     q.display();
     return 0;
 }
