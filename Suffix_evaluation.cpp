6646e87cc8f3be913b42c3148e3546d0178138e9
Ashish Garg <ashishgarg5006@gmail.com>
2023-05-08 11:24:20 +0530

    added comments

--- Suffix_evaluation.cpp
+++ Suffix_evaluation.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <stack>
-#include <string>
-#include <cmath>
+#include <stack>  // Required to use stack data structure
+#include <string> // Required to use string data type
+#include <cmath>  // Required to use pow() function for exponentiation
 
 using namespace std;
 
@@ -10,59 +10,59 @@ int performOperation(char op, int num1, int num2)
 {
     switch (op)
     {
-    case '+':
-        return num1 + num2;
-    case '-':
-        return num1 - num2;
-    case '*':
-        return num1 * num2;
-    case '/':
-        return num1 / num2;
-    case '%':
-        return num1 % num2;
-    case '^':
-        return pow(num1, num2);
+    case '+':                   // If the operator is addition
+        return num1 + num2;     // Return the sum of two numbers
+    case '-':                   // If the operator is subtraction
+        return num1 - num2;     // Return the difference of two numbers
+    case '*':                   // If the operator is multiplication
+        return num1 * num2;     // Return the product of two numbers
+    case '/':                   // If the operator is division
+        return num1 / num2;     // Return the quotient of two numbers
+    case '%':                   // If the operator is modulus
+        return num1 % num2;     // Return the remainder of two numbers
+    case '^':                   // If the operator is exponentiation
+        return pow(num1, num2); // Return the result of raising num1 to the power of num2
     }
-    return -1;
+    return -1; // If the operator is not recognized, return -1
 }
 
 // Function to evaluate a suffix expression
 int evaluateSuffixExpression(string exp)
 {
-    stack<int> stack;
-    for (int i = 0; i < exp.length(); i++)
+    stack<int> stack;                      // Create an empty stack of integers
+    for (int i = 0; i < exp.length(); i++) // Loop through each character of the string
     {
-        char c = exp[i];
-        if (isdigit(c))
+        char c = exp[i]; // Get the current character
+        if (isdigit(c))  // If the character is a digit
         {
             // Convert the character to integer and push onto the stack
-            stack.push(c - '0');
+            stack.push(c - '0'); // Convert the character to integer and push it onto the stack
         }
-        else
+        else // If the character is an operator
         {
             // Pop two operands from the stack and perform the operation
-            int num2 = stack.top();
-            stack.pop();
-            int num1 = stack.top();
-            stack.pop();
-            int result = performOperation(c, num1, num2);
+            int num2 = stack.top();                       // Get the top element of the stack and store it as num2
+            stack.pop();                                  // Pop the top element from the stack
+            int num1 = stack.top();                       // Get the new top element of the stack and store it as num1
+            stack.pop();                                  // Pop the new top element from the stack
+            int result = performOperation(c, num1, num2); // Perform the operation using num1, num2 and operator c, and store the result in result variable
 
             // Push the result back onto the stack
-            stack.push(result);
+            stack.push(result); // Push the result onto the stack
         }
     }
 
     // The final element on the stack is the result of the expression
-    return stack.top();
+    return stack.top(); // Return the top element of the stack, which is the result of the expression
 }
 
 // Main function to test the program
 int main()
 {
-    string exp;
-    cout << "Enter Suffix Expression: ";
-    getline(cin, exp);
-    int result = evaluateSuffixExpression(exp);
-    cout << "Result: " << result;
-    return 0;
+    string exp;                                 // Declare a string variable to store the suffix expression
+    cout << "Enter Suffix Expression: ";        // Prompt the user to enter the suffix expression
+    getline(cin, exp);                          // Read the input from the user and store it in the exp variable
+    int result = evaluateSuffixExpression(exp); // Evaluate the suffix expression and store the result in the result variable
+    cout << "Result: " << result;               // Print the result of the suffix expression
+    return 0;                                   // Return 0 to indicate successful execution of the program
 }
--- Tower_of_hanoi.cpp
+++ Tower_of_hanoi.cpp
@@ -1,27 +1,27 @@
-#include <iostream>
+#include <iostream>  
 using namespace std;
 
 void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod, int &moves)
-{
+{ // define the towerOfHanoi function that takes in four parameters: the number of disks (n), the rod the disks are currently on (from_rod), the rod the disks need to be moved to (to_rod), a spare rod that can be used to temporarily hold disks (aux_rod), and a reference to an integer that keeps track of the total number of moves (moves)
     if (n == 1)
-    {
-        moves++;
-        cout << "Move disk 1 from rod " << from_rod << " to rod " << to_rod << endl;
-        return;
+    {                                                                                // if there is only one disk to be moved
+        moves++;                                                                     // increment the total number of moves
+        cout << "Move disk 1 from rod " << from_rod << " to rod " << to_rod << endl; // print out the instruction to move the disk from the 'from' rod to the 'to' rod
+        return;                                                                      // exit the function
     }
-    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod, moves);
-    moves++;
-    cout << "Move disk " << n << " from rod " << from_rod << " to rod " << to_rod << endl;
-    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod, moves);
+    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod, moves);                                 // recursively call the towerOfHanoi function, moving n-1 disks from the 'from' rod to the 'auxiliary' rod, using the 'to' rod as the temporary rod, and passing in the same integer reference for moves
+    moves++;                                                                               // increment the total number of moves
+    cout << "Move disk " << n << " from rod " << from_rod << " to rod " << to_rod << endl; // print out the instruction to move the nth disk from the 'from' rod to the 'to' rod
+    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod, moves);                                 // recursively call the towerOfHanoi function, moving n-1 disks from the 'auxiliary' rod to the 'to' rod, using the 'from' rod as the temporary rod, and passing in the same integer reference for moves
 }
 
 int main()
-{
-    int n;
-    cout << "Enter the number of disks: ";
-    cin >> n;
-    int moves = 0;
-    towerOfHanoi(n, 'A', 'C', 'B', moves);
-    cout << "Total number of moves: " << moves << endl;
-    return 0;
+{                                                       
+    int n;                                              // declare an integer variable to store the number of disks
+    cout << "Enter the number of disks: ";              // prompt the user to enter the number of disks
+    cin >> n;                                           // read in the number of disks from the user
+    int moves = 0;                                      // initialize the total number of moves to 0
+    towerOfHanoi(n, 'A', 'C', 'B', moves);              // call the towerOfHanoi function with the number of disks, the 'from' rod as A, the 'to' rod as C, the 'auxiliary' rod as B, and the integer reference for moves
+    cout << "Total number of moves: " << moves << endl; // print out the total number of moves
+    return 0;                                           // exit the function
 }
