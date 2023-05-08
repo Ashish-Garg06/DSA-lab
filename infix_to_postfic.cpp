341d01b5089511435b7c474d0663414f4e3746b2
Ashish Garg <ashishgarg5006@gmail.com>
2023-05-08 11:35:33 +0530

    Added comments

--- infix_to_postfic.cpp
+++ infix_to_postfic.cpp
@@ -2,13 +2,14 @@
 #include <stack>
 using namespace std;
 
+// Function to return the precedence of the operator
 int F(char symbol)
 {
     switch (symbol)
     {
     case '+':
     case '-':
-        return 2;
+        return 2; 
     case '*':
     case '/':
         return 4;
@@ -23,6 +24,7 @@ int F(char symbol)
     }
 }
 
+// Function to return the priority of the symbol
 int G(char symbol)
 {
     switch (symbol)
@@ -43,45 +45,52 @@ int G(char symbol)
     }
 }
 
+// Function to convert infix expression to postfix expression
 void infix_postfix(char infix[], char postfix[])
 {
+    // Create a stack and push '#' into it
     stack<char> s;
     s.push('#');
     int j = 0;
+    // Traverse the infix expression
     for (int i = 0; infix[i] != '\0'; i++)
     {
         char symbol = infix[i];
+        // Pop and append the symbols with higher precedence from stack to postfix expression
         while (F(s.top()) > G(symbol))
         {
-            postfix[j] = s.top();
-            s.pop();
-            j++;
+            postfix[j] = s.top(); // Append the top symbol of stack to postfix expression
+            s.pop();              // Pop the top symbol from stack
+            j++;                  // Increment the index of postfix expression
         }
+        // Push the symbol onto the stack if its precedence is lower than the top of stack or if the stack is empty
         if (F(s.top()) != G(symbol))
         {
-            s.push(symbol);
+            s.push(symbol); // Push the symbol onto the stack
         }
         else
         {
-            s.pop();
+            s.pop(); // Pop the symbol if its precedence is equal to the top of stack
         }
     }
+    // Pop and append the remaining symbols from stack to postfix expression
     while (s.top() != '#')
     {
-        postfix[j] = s.top();
-        s.pop();
-        j++;
+        postfix[j] = s.top(); // Append the top symbol of stack to postfix expression
+        s.pop();              // Pop the top symbol from stack
+        j++;                  // Increment the index of postfix expression
     }
-    postfix[j] = '\0';
+    postfix[j] = '\0'; // Add null character at the end of postfix expression
 }
+
 
 int main()
 {
     char infix[20], postfix[20];
     cout << "Enter a valid infix expression: ";
-    cin.getline(infix, 20);
-    infix_postfix(infix, postfix);
-    cout << "The infix expression is: " << infix << endl;
-    cout << "The postfix expression is: " << postfix << endl;
+    cin.getline(infix, 20);                                   // Input the infix expression
+    infix_postfix(infix, postfix);                            // Convert infix expression to postfix expression
+    cout << "The infix expression is: " << infix << endl;     // Display the infix expression
+    cout << "The postfix expression is: " << postfix << endl; // Display the postfix expression
     return 0;
 }
