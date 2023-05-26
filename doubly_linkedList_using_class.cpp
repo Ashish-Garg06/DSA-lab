c8658931da7c2d7cb2995578f0845c9cbfe528a5
Ashish Garg <ashishgarg5006@gmail.com>
2023-05-26 18:26:46 +0530

    added linked list

---
+++ doubly_linkedList_using_class.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *front;
+    Node *back;
+
+    Node(int value)
+    {
+        data = value;
+        front = nullptr;
+        back = nullptr;
+    }
+};
+
+class Doubly_linked_list
+{
+private:
+    Node *Head;
+    Node *Tail;
+
+public:
+    Doubly_linked_list()
+    {
+        Head = nullptr;
+        Tail = nullptr;
+    }
+
+    // Insert a new node at the end of the list
+    void insert(int value)
+    {
+        Node *Newnode = new Node(value);
+        if (Head == nullptr)
+        {
+            // If the list is empty, set the new node as both the head and tail
+            Head = Newnode;
+            Tail = Newnode;
+        }
+        else
+        {
+            Node *current = Head;
+            while (current->front != nullptr)
+            {
+                // Traverse the list to find the last node
+                current = current->front;
+            }
+            current->front = Newnode;
+            Newnode->back = current;
+            Tail = Newnode; // Update the tail to the newly inserted node
+        }
+    }
+
+    // Display the list from the beginning (head to tail)
+    void displayfrombeginning()
+    {
+        Node *current = Head;
+        while (current != nullptr)
+        {
+            cout << current->data << " ";
+            current = current->front;
+        }
+        cout << endl;
+    }
+
+    // Display the list from the end (tail to head)
+    void displayfromend()
+    {
+        Node *current = Tail;
+        while (current != nullptr)
+        {
+            cout << current->data << " ";
+            current = current->back;
+        }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    Doubly_linked_list myList;
+
+    for (int i = 1; i <= 100; i++)
+    {
+        myList.insert(i);
+    }
+
+    myList.displayfrombeginning();
+
+    cout << "\n \n";
+    
+    myList.displayfromend();
+
+    return 0;
+}
---
+++ singly_ll_using_classes.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int value)
+    {
+        data = value;
+        next = nullptr;
+    }
+};
+
+class LinkedList
+{
+private:
+    Node *head;
+
+public:
+    LinkedList()
+    {
+        head = nullptr;
+    }
+
+    void insert(int value)
+    {
+        Node *newNode = new Node(value);
+
+        if (head == nullptr)
+        {
+            head = newNode;
+        }
+        else
+        {
+            Node *current = head;
+            while (current->next != nullptr)
+            {
+                current = current->next;
+            }
+            current->next = newNode;
+        }
+    }
+
+    void display()
+    {
+        Node *current = head;
+        while (current != nullptr)
+        {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    LinkedList myList;
+
+    myList.insert(5);
+    myList.insert(10);
+    myList.insert(15);
+    myList.insert(20);
+
+    myList.display();
+
+    return 0;
+}
