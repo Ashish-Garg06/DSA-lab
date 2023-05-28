d60c1f2095f86caee08015c9fdbdaff43c7f367c
Ashish Garg <ashishgarg5006@gmail.com>
2023-05-28 19:23:35 +0530

    added changes

--- doubly_linkedList_using_class.cpp
+++ doubly_linkedList_using_class.cpp
@@ -29,6 +29,14 @@ public:
         Tail = nullptr;
     }
 
+    // Insert a new node at the beginning of the list
+    void insert_at_beginning(int value)
+    {
+        Node *Newnode = new Node(value);
+        Newnode->front = Head;
+        Newnode->back = nullptr;
+        Head = Newnode;
+    }
     // Insert a new node at the end of the list
     void insert_at_end(int value)
     {
@@ -53,6 +61,52 @@ public:
         }
     }
 
+    void insertAtPosition(int data, int position)
+    {
+        if (position <= 0)
+        {
+            insert_at_beginning(data);
+            return;
+        }
+
+        Node *newNode = new Node(data);
+        Node *current = Head;
+        Node *previous = nullptr;
+        int count = 1;
+
+        while (current != nullptr && count < position)
+        {
+            previous = current;
+            current = current->front;
+            count++;
+        }
+
+        newNode->front = current;
+        current->back = newNode;
+        if (previous != nullptr)
+        {
+            previous->front = newNode;
+            newNode->back = previous;
+        }
+        else
+        {
+            Head = newNode;
+        }
+    }
+
+    void deleteAtBeginning()
+    {
+        if (Head == nullptr)
+        {
+            cout << "List is empty. Nothing to delete." << endl;
+            return;
+        }
+
+        Node *temp = Head;
+        Head = Head->front;
+        delete temp;
+    }
+
     // Display the list from the beginning (head to tail)
     void displayfrombeginning()
     {
@@ -65,6 +119,91 @@ public:
         cout << endl;
     }
 
+    void deleteAtEnd()
+    {
+        if (Head == nullptr)
+        {
+            cout << "List is empty. Nothing to delete." << endl;
+            return;
+        }
+
+        Node *current = Head;
+        Node *previous = nullptr;
+
+        while (current->front != nullptr)
+        {
+            previous = current;
+            current = current->front;
+        }
+
+        if (previous != nullptr)
+        {
+            previous->front = nullptr;
+        }
+        else
+        {
+            Head = nullptr;
+        }
+
+        delete current;
+    }
+
+    // Delete node at a specific position in the list
+    void deleteAtPosition(int position)
+    {
+        if (Head == nullptr)
+        {
+            cout << "List is empty. Nothing to delete." << endl;
+            return;
+        }
+
+        if (position <= 0)
+        {
+            deleteAtBeginning();
+            return;
+        }
+
+        Node *current = Head;
+        Node *previous = nullptr;
+        int count = 1;
+
+        while (current != nullptr && count < position)
+        {
+            previous = current;
+            current = current->front;
+            count++;
+        }
+
+        if (current == nullptr)
+        {
+            cout << "Invalid position. Node not found." << endl;
+            return;
+        }
+
+        if (previous != nullptr)
+        {
+            previous->front = current->front;
+            current->back=previous;
+        }
+        else
+        {
+            Head = current->front;
+        }
+
+        delete current;
+    }
+
+    int countNodes()
+    {
+        Node *current = Head;
+        int count = 0;
+        while (current != nullptr)
+        {
+            count++;
+            current = current->front;
+        }
+        return count;
+    }
     // Display the list from the end (tail to head)
     void displayfromend()
     {
@@ -82,16 +221,28 @@ int main()
 {
     Doubly_linked_list myList;
 
-    for (int i = 1; i <= 100; i++)
-    {
-        myList.insert_at_end(i);
-    }
+    myList.insert_at_beginning(3);
+    myList.insert_at_beginning(2);
+    myList.insert_at_beginning(1);
 
+    myList.insert_at_end(4);
+    myList.insert_at_end(5);
+
+    myList.insertAtPosition(10, 2);
+
+    cout << "Linked List after insertions: ";
     myList.displayfrombeginning();
 
-    cout << "\n \n";
+    myList.deleteAtPosition(2);
+    myList.displayfrombeginning();
 
-    myList.displayfromend();
+
+    cout << "Linked List after deletions: ";
+    myList.displayfrombeginning();
+
+    int nodeCount = myList.countNodes();
+    cout << "Number of nodes in the list: " << nodeCount << endl;
+
 
     return 0;
 }
